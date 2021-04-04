f7b89aab55fe9ef8327358b9cd7cfc7742c8e24c
Naoki Nagakura <nagakura@nagakuranaokis-MacBook-Pro.local>
2021-04-04 21:24:40 +0900

    create pichart graph

--- app/src/main/java/com/example/swipepoll/MainActivity.kt
+++ app/src/main/java/com/example/swipepoll/MainActivity.kt
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
+import android.graphics.Color
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Spinnerの設定
         val spinnerItems = arrayOf(
             "Select your question",
             "Which one do you like?",
@@ -22,8 +29,35 @@ class MainActivity : AppCompatActivity() {
             spinnerItems
         )
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
         val spinner = findViewById<Spinner>(R.id.spinner3)
         spinner.adapter = adapter
+
+        // Graph
+        val dimensions = listOf("TOYOTA", "NISSAN")//分割円の名称(String型)
+        val values = listOf(4f, 1f)//分割円の大きさ(Float型)
+        val bgcolors = listOf(Color.RED, Color.BLUE)
+        //val bgcolors = listOf(Color.rgb(192,0,0), Color.rgb(0,192,0))
+        //①Entryにデータ格納
+        var entryList = mutableListOf<PieEntry>()
+        for(i in values.indices){
+            entryList.add(
+                PieEntry(values[i], dimensions[i])
+            )
+        }
+        //②PieDataSetにデータ格納
+        val pieDataSet = PieDataSet(entryList, "candle")
+        //③DataSetのフォーマット指定
+        //pieDataSet.colors = ColorTemplate.COLORFUL_COLORS.toList()
+        pieDataSet.colors = bgcolors
+        //④PieDataにPieDataSet格納
+        val pieData = PieData(pieDataSet)
+        //⑤PieChartにPieData格納
+        val pieChart = findViewById<PieChart>(R.id.pieChartExample)
+        pieChart.data = pieData
+        //⑥Chartのフォーマット指定
+        pieChart.legend.isEnabled = false
+        pieChart.setEntryLabelTextSize(20f)
+        //⑦PieChart更新
+        pieChart.invalidate()
     }
 }
