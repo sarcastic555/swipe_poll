2e2c4b8316ab4be270388a9e57abb8a24610afad
Naoki Nagakura <nagakura@nagakuranaokis-MacBook-Pro.local>
2021-04-04 17:07:19 +0900

    spinner template success

--- app/src/main/java/com/example/swipepoll/MainActivity.kt
+++ app/src/main/java/com/example/swipepoll/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.swipepoll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinnerItems = arrayOf(
+            "Spinner",
+            "Android",
+            "Apple",
+            "Windows"
+        )
+        val adapter = ArrayAdapter(
+            applicationContext,
+            android.R.layout.simple_spinner_item,
+            spinnerItems
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val spinner = findViewById<Spinner>(R.id.spinner3)
+        spinner.adapter = adapter
+        //val adapter =
+        //ArrayAdapter<String>(this, R.layout.simple_spinner_item)
+        //adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        //adapter.add("A型")
+        //adapter.add("B型")
+        //adapter.add("AB型")
+        //adapter.add("O型")
+        //val spinner = findViewById<View>(R.id.spinner3) as Spinner
+        //spinner.adapter = adapter
     }
 }
