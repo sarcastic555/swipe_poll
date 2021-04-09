b0fc6674826cb6ad65a239da6c1fe71a3ad75695
Naoki Nagakura <nagakura@nagakuranaokis-MacBook-Pro.local>
2021-04-09 22:59:13 +0900

    set hourglass when pushing button

--- app/src/main/java/com/example/swipepoll/MainActivity.kt
+++ app/src/main/java/com/example/swipepoll/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.swipepoll
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.Spinner
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
-import com.github.mikephil.charting.utils.ColorTemplate
-import android.graphics.Color
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setScreenMain();
+    }
+
+    private fun setScreenMain() {
         setContentView(R.layout.activity_main)
+        var progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        progressBar.visibility = View.GONE
 
         // Spinnerの設定
         val spinnerItems = arrayOf(
@@ -28,6 +38,40 @@ class MainActivity : AppCompatActivity() {
             R.layout.spinner_item,
             spinnerItems
         )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val spinner = findViewById<Spinner>(R.id.spinner3)
+        spinner.adapter = adapter
+
+        class TextChangeRunnable() : Runnable {
+            override fun run() {
+                setScreenSub()
+            }
+        }
+
+        val createButton = findViewById<Button>(R.id.button3);
+        createButton.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            val handler = Handler()
+            handler.postDelayed(TextChangeRunnable(), 3000)
+        };
+    }
+
+    private fun setScreenSub() {
+        setContentView(R.layout.activity_sub)
+        // Spinnerの設定
+        val spinnerItems = arrayOf(
+            "Select your question",
+            "Which one do you like?",
+            "Which one do you use more often?",
+            "Which one do you have?"
+        )
+        val adapter = ArrayAdapter(
+            applicationContext,
+            R.layout.spinner_item,
+            spinnerItems
+        )
+
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         val spinner = findViewById<Spinner>(R.id.spinner3)
         spinner.adapter = adapter
@@ -58,5 +102,8 @@ class MainActivity : AppCompatActivity() {
         pieChart.setEntryLabelTextSize(20f)
         //⑦PieChart更新
         pieChart.invalidate()
+
+        val createButton = findViewById<Button>(R.id.button3);
+        createButton.setOnClickListener {setScreenMain()};
     }
 }
